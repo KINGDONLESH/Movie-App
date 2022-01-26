442640e8e390980d4bedaaa53272d0e80f12421b
ZiziphoMa <zizipho.magugwana@dadevs.co.za>
2022-01-26 16:57:17 +0200

    Pull from branch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { ngbCarouselTransitionOut } from '@ng-bootstrap/ng-bootstrap/carousel/ca
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'string';
   viewmore: number = 10;
   moviesListItems!: number[];
@@ -22,9 +21,6 @@ export class AppComponent {
  viewLessTogggle(): void{
   this.viewLess = this.viewmore -5;
   }
-=======
-  title = 'MovieDB';
->>>>>>> 954c4a6855f4b36603bb11bd75eddbfa43c0a759
 }
 
 function viewLessTogggle() {
