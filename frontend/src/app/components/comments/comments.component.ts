0aff2adeb4fd65fd10ecca14b795670f7f01b903
ZiziphoMa <zizipho.magugwana@dadevs.co.za>
2022-01-27 17:02:31 +0200

    busy with comments buttun

--- frontend/src/app/components/comments/comments.component.ts
+++ frontend/src/app/components/comments/comments.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+// import { getSyntheticLeadingComments } from 'typescript';
+import {CommentService} from '../../services/comment.service'
 
 @Component({
   selector: 'app-comments',
@@ -6,12 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
+  // commentsArr: any = [];
+  // results: any = [];
+  // title: any;
+  // numberOfComments: number = 10;
+  // commentsTobeDisplayed: any;
+
 
   username: any = "Shiba username";
   comment: any = "Koenaite comment";
-  constructor() { }
+  getComments: any;
+  constructor(private CommentApi: CommentService) { 
+    // this.commentsArr = []
+
+
+
+   }
 
   ngOnInit(): void {
+    this.getComments();
+  
   }
-
 }
+
+// getComments(): void{
+//   this.commentApi: Comment()
+//   .subscribe((res:any)) =>
+//   this.commentsArr = res.resuls.splice(0, this.numberOFComments)
+
+
+
+
+  
+// }
+
+
+
+
+// function getComments() {
+//   throw new Error('Function not implemented.');
+// }
