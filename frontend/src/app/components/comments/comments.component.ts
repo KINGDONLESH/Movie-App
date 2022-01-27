675ce63c82fcf5a5f2789404d6b64cc8bec4d70f
ZiziphoMa <zizipho.magugwana@dadevs.co.za>
2022-01-27 22:08:50 +0200

    Added button to toggle comments

--- frontend/src/app/components/comments/comments.component.ts
+++ frontend/src/app/components/comments/comments.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
 // import { getSyntheticLeadingComments } from 'typescript';
 import {CommentService} from '../../services/comment.service'
 
@@ -24,6 +25,11 @@ const list = [
       date: '1days ago',
       message: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos sit error voluptatibus veniam exercitationem veritatis nisi dolores eos earum itaque illum quae sapiente, iste unde quo ab assumenda. Totam, harum.'
     },
+    {
+      name: 'Simphiwe',
+      date: '3 days ago',
+      message: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos sit error voluptatibus veniam exercitationem veritatis nisi dolores eos earum itaque illum quae sapiente, iste unde quo ab assumenda. Totam, harum.'
+    }
 ]; 
 
 @Component({
@@ -32,27 +38,42 @@ const list = [
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
-  // commentsArr: any = [];
-  // results: any = [];
-  // title: any;
-  // numberOfComments: number = 10;
-  // commentsTobeDisplayed: any;
-
-
+  displayAllComments = false;
   username: any = "Shiba username";
   comment: any = "Koenaite comment";
   getComments: any;
   testArr: any = list
+  allComments: any = list
+
   constructor(private CommentApi: CommentService) { 
-    // this.commentsArr = []
-
-
-
-   }
+  
+  }
 
   ngOnInit(): void {
-    this.getComments();
+    //this.getComments();
   
+  }
+
+  toggleItems(){
+    const half = Math.ceil(this.allComments.length / 2);    
+    const firstHalf = this.allComments.slice(0, half);
+
+    // Update list to display
+    if(this.displayAllComments){
+      // console.log('Display all: 1'  + this.allComments);
+      // console.log('Display all: 2'  + JSON.stringify(this.allComments));
+
+      // console.log(`Display all: 3: ${this.allComments}`);
+      // console.log(`Display all: 4: ${JSON.stringify(this.allComments)}`);
+
+      this.testArr = this.allComments;
+    }else{
+      this.testArr = firstHalf;
+      //console.log('Display half: ' + JSON.stringify(firstHalf));
+    }
+
+    this.displayAllComments = !this.displayAllComments;
+
   }
 }
 
