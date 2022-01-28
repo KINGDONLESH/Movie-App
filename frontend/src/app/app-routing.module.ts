a0c9d680584b0cc26f3c86cb6e0d223234c20372
simphiwe <simphiwe.nkabinde@dadevs.co.za>
2022-01-28 15:28:59 +0200

    Added the logo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsPageComponent } from './components/details-page/details-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { MoviesListItemsComponent } from './components/movies-list-items/movies-list-items.component';
 import { SignupComponent } from './components/signup/signup.component';
 
@@ -9,8 +10,7 @@ const routes: Routes = [
   {path: '', component: MoviesListItemsComponent},
   {path: 'details',component: DetailsPageComponent },
   {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '', component: MoviesListItemsComponent}
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
